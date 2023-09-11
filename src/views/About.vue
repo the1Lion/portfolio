9f6a5a8c2fedfce480a9133763d40aa346c318cf
mkatramez <m.katramez@schubwerk.de>
2023-09-11 13:15:38 +0200

    Adding transition with global style

--- src/views/About.vue
+++ src/views/About.vue
@@ -26,15 +26,4 @@ export default {
 </template>
 
 <style scoped>
-
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -13,9 +13,11 @@ export default {
       class="bg-gradient-to-r from-cyan-500 to-blue-500 dark:bg-gradient-to-r dark:from-violet-500 dark:to-fuchsia-500">
     <div class="container mx-auto flex flex-col h-screen">
       <Navigation/>
+      <transition appear :duration="550" name="fade">
       <div class="flex-grow container lg:rounded-2xl px-12 py-12 bg-white dark:bg-[#111111] dark:text-white">
         <h1>Resume</h1>
       </div>
+      </transition>
       <Footer/>
     </div>
   </div>
