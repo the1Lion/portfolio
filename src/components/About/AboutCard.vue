8e609aea632c09f414bb421fc13460854798c07d
mkatramez <m.katramez@schubwerk.de>
2023-09-12 08:16:21 +0200

    create new aboutcarcomponent

---
+++ src/components/About/AboutCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "AboutCard",
+  props: {
+    cardTitle: '',
+    cardDescription: ''
+  }
+}
+</script>
+
+<template>
+  <a href="/" class="block max-w-sm p-6 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ cardTitle }}</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">{{ cardDescription }}</p>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/About/AboutDescription.vue
+++ src/components/About/AboutDescription.vue
@@ -1,6 +1,9 @@
 <script>
+import AboutCard from "./AboutCard.vue";
+
 export default {
-  name: "AboutDescription"
+  name: "AboutDescription",
+  components: {AboutCard}
 }
 </script>
 
@@ -15,26 +18,10 @@ export default {
 
     <p class="text-3xl mt-5 mb-5">My abilities </p>
       <div class="flex flex-wrap gap-5 justify-center">
-
-        <a href="/" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </a>
-
-        <a href="/" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </a>
-
-
-        <a href="/" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </a>
-        <a href="/" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </a>
+        <AboutCard card-title="Noteworthy technology acquisitions 2021" card-description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
+        <AboutCard card-title="Noteworthy technology acquisitions 2021" card-description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
+        <AboutCard card-title="Noteworthy technology acquisitions 2021" card-description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
+        <AboutCard card-title="Noteworthy technology acquisitions 2021" card-description="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."/>
       </div>
   </div>
 </template>
