ba084d799ab4dfc54cd439a88a0cd340fc6c1b53
mkatramez <m.katramez@schubwerk.de>
2023-09-13 08:59:06 +0200

    transition ProfileImage.vue

---
+++ src/components/Profile/ProfileImage.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "ProfileImage"
+}
+</script>
+
+<template>
+  <a :download=$constants.USER_IMAGE :href=$constants.USER_IMAGE title="Download Image">
+    <img
+        :src=$constants.USER_IMAGE
+        class="mx-auto mb-4 w-36 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+        alt="Avatar"/>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Profile/ProfileInformation.vue
+++ src/components/Profile/ProfileInformation.vue
@@ -1,10 +1,11 @@
 <script>
 import ProfileSocialIcons from "./ProfileSocialIcons.vue";
 import ProfileContactInformation from "./ProfileContactInformation.vue";
+import ProfileImage from "./ProfileImage.vue";
 
 export default {
   name: "ProfileInformation",
-  components: {ProfileContactInformation, ProfileSocialIcons},
+  components: {ProfileImage, ProfileContactInformation, ProfileSocialIcons},
 }
 </script>
 
@@ -12,12 +13,7 @@ export default {
 
   <div class="block max-w-fit rounded-2xl px-12 py-12 bg-white dark:bg-[#111111] dark:text-white">
     <div class="mb-5 text-center">
-      <a :download=$constants.USER_IMAGE :href=$constants.USER_IMAGE title="Download Image">
-        <img
-            :src=$constants.USER_IMAGE
-            class="mx-auto mb-4 w-36 rounded-lg"
-            alt="Avatar"/>
-      </a>
+      <ProfileImage/>
       <h5 class="mb-2 text-xl font-medium leading-tight">{{ $constants.USER_NAME }}</h5>
       <p class="text-neutral-500 dark:text-neutral-400">{{ $constants.USER_JOB }}</p>
     </div>
